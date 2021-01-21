4dce847df9d1809f6d8aac6dfccf9ce5ac32eae4
pfiadDi <47104478+pfiadDi@users.noreply.github.com>
2021-01-21 15:07:39 +0100

    unnecessary check for database url in config rmvd

--- resources/js/config.js
+++ resources/js/config.js
@@ -7,8 +7,6 @@ module.exports = {
 
         if (config.apiKey === undefined || config.apiKey === "") return status.returnStatus("error", "Config file: no valid API key");
 
-        if (config.databaseURL === undefined || config.databaseURL === "") return status.returnStatus("error", "Config file: no valid databaseUrl");
-
         if (config.projectId === undefined || config.projectId === "") return status.returnStatus("error", "Config file: no valid projectId");
 
         if (config.appId === undefined || config.appId === "") return status.returnStatus("error", "Config file: no valid appId");
